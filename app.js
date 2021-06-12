3c4604ad90a57a8af9815fa947d292f0d3555ad3
Trijeet Ganguly <trijeet.ganguly@gmail.com>
2021-06-12 11:53:09 +0530

    Add basic images

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import {constants} from './src/constants.js';
+
 var config = {
     type: Phaser.AUTO,
     width: 800,
@@ -18,30 +20,22 @@ var game = new Phaser.Game(config);
 
 function preload ()
 {
-    this.load.setBaseURL('https://labs.phaser.io');
-
-    this.load.image('sky', 'assets/skies/space3.png');
-    this.load.image('logo', 'assets/sprites/phaser3-logo.png');
-    this.load.image('red', 'assets/particles/red.png');
+    this.load.image('map', './assets/maps/map-cafeteria.png');
+    this.load.spritesheet(
+        'player',
+        './assets/sprites/walk/walk-combined.png',
+        {
+            frameHeight: constants['PLAYER_SPRITE_HEIGHT'],
+            frameWidth: constants['PLAYER_SPRITE_WIDTH']
+        }
+    );
 }
 
 function create ()
 {
-    this.add.image(400, 300, 'sky');
+    this.add.image(400, 300, 'map');
+    const player = this.add.sprite(350, 450, 'player');
+    player.displayHeight = constants['PLAYER_HEIGHT'];
+    player.displayWidth = constants['PLAYER_WIDTH'];
 
-    var particles = this.add.particles('red');
-
-    var emitter = particles.createEmitter({
-        speed: 100,
-        scale: { start: 1, end: 0 },
-        blendMode: 'ADD'
-    });
-
-    var logo = this.physics.add.image(400, 100, 'logo');
-
-    logo.setVelocity(100, 200);
-    logo.setBounce(1, 1);
-    logo.setCollideWorldBounds(true);
-
-    emitter.startFollow(logo);
 }
---
+++ src/constants.js
@@ -0,0 +1,6 @@
+export const constants = {
+    "PLAYER_SPRITE_HEIGHT": 334,
+    "PLAYER_SPRITE_WIDTH": 250,
+    "PLAYER_HEIGHT": 66,
+    "PLAYER_WIDTH": 50,
+}
